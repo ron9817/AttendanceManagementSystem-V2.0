27a86767157a3df4363e4392093276db8d1e10fa
Owner <djangoronkar@4261811f904c>
2019-09-10 16:29:45 +0000

    Kind of deone with the basic version-- connected database, all the result are from database which means real data. Now its a break remaining functionality will be done afterwards. Meanwhile the current functionality developed will be tested

--- attendance_controller/views.py
+++ attendance_controller/views.py
@@ -5,6 +5,8 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 import datetime
 
+from .models import Dailyattendance
+
 
 # Create your views here.
 #0-total
@@ -23,20 +25,36 @@ attendance={
     "N/W lab":[0,0,0,0],
     "AndroidLab":[0,0,0,0]
 }
-def index(request):
-    # today=datetime.date.today()
 
-    #datetime.date(year, month, day)
-    timetable={
-        "Mon":["STQA","AI","END","InfraSec","Project A"],
+monthMap={
+    'Jan':'01',
+    'Feb':'02',
+    'Mar':'03',
+    'Apr':'04',
+    'May':'05',
+    'Jun':'06',
+    'Jul':'07',
+    'Aug':'08',
+    'Sep':'09',
+    'Oct':'10',
+    'Nov':'11',
+    'Dec':'12'
+    }
+timetable={
+        "Mon":["Project A"],
         "Tue":["CSL","InfraSec","AI","IS","END","STQA"],
         "Wednes":["CSL","END","AdvSec","AI","InfraSec"],
-        "Thurs":["InfraSec","AI","N/W lab","END","CSL"],
-        "Fri":["END","AI","InfraSec","AndroidLab","STQA","STQA"],
+        "Thurs":["AI","AI","InfraSec","STQA","N/W lab","END","CSL"],
+        "Fri":["AI","InfraSec","STQA","STQA","AndroidLab","END","END"],
         "Sat":[],
         "Sun":[]
     }
-    weekMap=["Mon","Tue","Wednes","Thurs","Fri","Sat","Sun"]
+weekMap=["Mon","Tue","Wednes","Thurs","Fri","Sat","Sun"]
+
+def index(request):
+    # today=datetime.date.today()
+    #datetime.date(year, month, day)
+
     if (request.method=="POST"):
         date=request.POST["changeDate"]
         year=int(date[0:4])
@@ -45,6 +63,12 @@ def index(request):
         #print(day,month,year)
         #return HttpResponse(1)
         weekDay=datetime.date(year,month,day).weekday()
+        attendance={}
+        for subject in timetable[weekMap[weekDay]]:
+            Qs=Dailyattendance.objects.filter(subject=subject).exclude(ispresent=0)
+            totalLec=len(Qs)
+            attendedLec=len(Qs.filter(ispresent=1))
+            attendance[subject]=[totalLec,attendedLec,0,0]
         #print(day,month,year,weekDay)
         #return HttpResponse(1)
     else:
@@ -53,6 +77,12 @@ def index(request):
         month=today.month
         day=today.day
         weekDay=today.weekday()
+        attendance={}
+        for subject in timetable[weekMap[weekDay]]:
+            Qs=Dailyattendance.objects.filter(subject=subject).exclude(ispresent=0)
+            totalLec=len(Qs)
+            attendedLec=len(Qs.filter(ispresent=1))
+            attendance[subject]=[totalLec,attendedLec,0,0]
         #print(day,month,year,weekDay)
     context={
         "timeTable":timetable,
@@ -61,30 +91,39 @@ def index(request):
         "attendance":attendance,
         "proxySubject":set(attendance.keys())-set(timetable[weekMap[weekDay]])
         }
-
     return render(request, "attendance_controller/index.html",context)
 @csrf_exempt
 def calculate(request):
     data = json.loads(request.body)
-    print(data)
+    date=data["date"]
+    year=date[11:15]
+    day=date[8:10]
+    month=date[4:7]
+    date=year+"-"+monthMap[month]+"-"+day
     lecPresent=data["present"]
     lecAbsent=data["absent"]
     lecOff=data["off"]
     lecProxy=data["proxy"]
     print(lecPresent,lecAbsent,lecOff,lecProxy)
     for i in lecPresent:
+        attendanceRow=Dailyattendance(ispresent=1, subject=i, date=date)
+        attendanceRow.save()
         attendance[i][0]+=1
         attendance[i][1]+=1
     for i in lecAbsent:
+        attendanceRow=Dailyattendance(ispresent=-1, subject=i, date=date)
+        attendanceRow.save()
         attendance[i][0]+=1
     for i in lecOff:
+        attendanceRow=Dailyattendance(ispresent=0, subject=i, date=date)
+        attendanceRow.save()
         attendance[i][2]+=1
     for i in lecProxy:
+        attendanceRow=Dailyattendance(ispresent=1, subject=i, date=date)
+        attendanceRow.save()
         attendance[i][0]+=1
         attendance[i][1]+=1
         attendance[i][3]+=1
-    print("\nAttendance\n")
-    print(attendance)
     return JsonResponse({
-        "msg":"successfully marked attendance for "+data["date"]
+        "msg":"successfully marked attendance for "+date
     })
